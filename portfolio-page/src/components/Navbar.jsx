8ee18a0122a593cf6e889410110fa32aaf33d4b6
James Hoofard <james.hoofard.iii@gmail.com>
2023-06-04 15:27:42 -0500

    added list to nav bar

--- portfolio-page/src/components/Navbar.jsx
+++ portfolio-page/src/components/Navbar.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import letterH from '../assets/clipart-h.png'
 import {FaBars, FaTimes} from 'react-icons/fa'
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
   return (
-    <div className='fixed w-full h-[90px] flex justify-between items-center px-4 bg-[#0a192f] text-grey-300'>
+    <div className='fixed w-full h-[90px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
     <div>
     <img src={letterH} alt="letter H"style={{width: '50px', height: '50px'}} />
+ 
+ {/*menu*/}
+
+    
+      <ul className='hidden md:flex'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+    {/*hamburger*/}
+    <div className='md:hidden z-10'>
+    <FaBars/>
+    </div>
+{/*mobile menu*/}
+    <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+        <li className='py-6 text-4xl'>Home</li>
+        <li className='py-6 text-4xl'>About</li>
+        <li className='py-6 text-4xl'>Skills</li>
+        <li className='py-6 text-4xl'>Work</li>
+        <li className='py-6 text-4xl'>Contact</li>
+    </ul>
+
+    {/*Social icons*/}
+    <div className='hidden'>
+
+    </div>
     </div>
     </div>
   )
